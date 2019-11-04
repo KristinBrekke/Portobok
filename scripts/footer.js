705584548a49152c6e91448a8fbd6d31713917da
KristinBrekke <54547408+KristinBrekke@users.noreply.github.com>
2019-11-04 12:06:19 +0100

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -10,11 +10,11 @@ document.getElementById("footer-placeholder").innerHTML =
 <body>
     
     <footer>
-        <div id="blokk">
+        <div id="footer">
         <p class="contact_information">Kontaktinformasjon:</p>
-        <div id="email"><p class="contact_information">portobok@portobok.com</p></div>
-        <div id="phone"><p class="contact_information">Telefon: 12345678</p></div>
-        <div id="adress"><p class="contact_information">Adresse: Trondheimsveien 4, 7030 Trondheim, Norge</p></div>
+        <div id="email"><a href="mailto:portobok@portobok.com"><p class="contact_information">portobok@portobok.com</p></a></div>
+        <div id="phone"><a href="tel:12345678"><p class="contact_information">Telefon: 12345678</p></a></div>
+        <div id="adress"><a href="https://www.google.com/maps/place/Produktdesign,+7034+Trondheim/@63.4159019,10.4116094,14.95z/data=!4m5!3m4!1s0x466d319583c05947:0x88b69f4c40fb20a1!8m2!3d63.4180732!4d10.4049145"><p class="contact_information">Adresse: Produktdesign, 7034 Trondheim, Norge</p></a></div>
         </div>
     </footer>
 </body>
