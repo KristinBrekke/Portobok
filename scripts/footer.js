e999c3ac5991fdcd5447bc582178963d2ad6edc5
KristinBrekke <54547408+KristinBrekke@users.noreply.github.com>
2019-11-04 10:46:12 +0100

    Bilder i landsskapsformat

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,20 +1,21 @@
 // JavaScript Document
 document.getElementById("footer-placeholder").innerHTML = 
     `<html>
-        <head>
-            <meta charset="utf-8">
-            <title>Footer</title>
-                <link rel="stylesheet" type="text/css" href="../style.css">
-        </head>
+<head>
+<meta charset="utf-8">
+<title>Footer</title>
+    <link rel="stylesheet" type="text/css" href="../style.css">
+</head>
 
-        <body>
+<body>
     
-            <footer>
-                <div id="navigasjonsbar">
-        
-                </div>
+    <footer>
+        <div id="blokk">
+        <p>Kontaktinformasjon</p>
+        <p>E-post: portobok@portobok.com Telefon: 12345678 Adresse: Trondheimsveien 4, 7030 Trondheim Norge</p>
+        </div>
     
-            </footer>
-        </body>
-        </html>
+    </footer>
+</body>
+</html>
     `
