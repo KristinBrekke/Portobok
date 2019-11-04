bf57745a1e1c43fd148461292f749e9e8ec9732e
KristinBrekke <54547408+KristinBrekke@users.noreply.github.com>
2019-11-04 11:28:15 +0100

    Putter på navbaren

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -11,14 +11,28 @@ document.getElementById("navbar-placeholder").innerHTML =
 <body>
     <header>
         <div id="blokk">
-        <ul class="navbar">
+        <label for= "box">&#9776</label>
+			<input type = "checkbox" id ="box"/>
+			<ul class="navbar">
             <li><a href="../html/homepage.html"> <img src="../images/logo_pb.png" alt="logo Portobok" id="logo_portobok"></a></li>
             <li><a href="../html/designers.html">DESIGNERE</a></li>
             <li><a href="../html/projects.html">PROSJEKTER</a></li>
             <li><a href="../html/contact.html">KONTAKT</a></li>
         </ul>
     </div>
+	
+	 <!--solveig putter inn hamburgerkode her -->
+		<!--<label for= "box">&#9776</label>
+			<input type = "checkbox" id ="box"/>-->
+		<!--kan bruke koden som allerede er her-->
+			<!--<div class="hamburger">
+            <a href="../html/homepage.html"> <img src="../images/logo_pb.png" alt="logo Portobok" id="logo_portobok2"></a>
+            <a href="../html/designers.html">DESIGNERE</a>
+			<a href="../html/projects.html">PROSJEKTER</a>
+            <a href="#">KONTAKT</a>
+			</div>-->
     </header>
+<script href= "navbar.js"> </script>	
 </body>
 </html>
     `
