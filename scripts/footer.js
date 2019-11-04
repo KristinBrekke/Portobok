934ea4a4bf50b1f1f970d09ab1c3265f756479c0
KristinBrekke <54547408+KristinBrekke@users.noreply.github.com>
2019-11-04 15:53:38 +0100

    Update footer.js

--- scripts/footer.js
+++ scripts/footer.js
@@ -1,22 +1,11 @@
 // JavaScript Document
 document.getElementById("footer-placeholder").innerHTML = 
-    `<html>
-<head>
-<meta charset="utf-8">
-<title>Footer</title>
-    <link rel="stylesheet" type="text/css" href="../style.css">
-</head>
-
-<body>
-    
-    <footer>
+    `
         <div id="footer">
         <p class="contact_information">Kontaktinformasjon:</p>
         <div id="email"><a href="mailto:portobok@portobok.com"><p class="contact_information">E-post: portobok@portobok.com</p></a></div>
         <div id="phone"><a href="tel:12345678"><p class="contact_information">Telefon: 12345678</p></a></div>
         <div id="adress"><a href="https://www.google.com/maps/place/Produktdesign,+7034+Trondheim/@63.4159019,10.4116094,14.95z/data=!4m5!3m4!1s0x466d319583c05947:0x88b69f4c40fb20a1!8m2!3d63.4180732!4d10.4049145"><p class="contact_information">Adresse: Produktdesign, 7034 Trondheim, Norge</p></a></div>
         </div>
-    </footer>
-</body>
-</html>
     `
+//<link rel="stylesheet" type="text/css" href="../style.css"> Ha med denne?
