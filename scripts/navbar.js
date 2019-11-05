afb73010995a61ddf6c61cf6a6a7a5d4670ca63d
Solveig Waagbø Kavli <solveig.w.kavli@gmail.com>
2019-11-05 22:02:24 +0100

    litt på navbar

--- scripts/navbar.js
+++ scripts/navbar.js
@@ -1,6 +1,6 @@
 // JavaScript Document
 
-document.getElementById("navbar-placeholder").innerHTML = 
+//document.getElementById("navbar-placeholder").innerHTML = 
     `<html>
 <head>
 <meta charset="utf-8">
@@ -56,21 +56,37 @@ function hamburger(){
 function hamburger(){
 	var x = document.getElementById("box");
 	var navbar = document.getElementsByClassName("navbar");
+	var y = window.matchMedia("(min-width: 940px)");
+	
 	
 	for (var i=0; i < navbar.length; i++){
+			
 		if (x.checked){
-			console.log("hallo");
-			navbar[i].style.display ="block"; //gjør at listeelementene legger seg på linje nedover
-			//navbar.style.flexDirection: "column";
-			navbar[i].style.float= "none"; //gjør at listen ikke lenger legger seg bortover
+			
+			navbar[i].style.display ="block"; //skulle gjør at listeelementene legger seg på rad nedover
+			//navbar.style.flexDirection= "column";
+			//navbar[i].style.float= "none"; //gjør at listen ikke lenger legger seg bortover
+			console.log(navbar[i]);
+			if (y.matches){ //dette skal være om skjermen er større enn 940px
+				navbar[i].style.display ="inline"
+				console.log("heia");
+			}
 		}
+		
 		else {
 			navbar[i].style.display = "none" //gjør at listeleementene ikke vises
 			console.log("hei")
-			navbar[i].style.float = "left";
+			
+			if (y.matches){ //dette skal være om skjermen er større enn 940px
+				navbar[i].style.display ="inline"
+				console.log("heia");
+			}
+		}
+			
+			
 		}
 		
-	}
+	
 	
 	
 	
