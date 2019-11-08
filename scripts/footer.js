5ffbc36f0b6fd0e282983b9beac2099128c8bcf2
oekeller <55976310+oekeller@users.noreply.github.com>
2019-11-08 20:54:34 +0100

    adresse

--- scripts/footer.js
+++ scripts/footer.js
@@ -5,7 +5,7 @@ document.getElementById("footer-placeholder").innerHTML =
 		
 		<a href="../html/homepage.html" id="footer_logo"><img src="../images/logo_pb_tykkere.png" alt="Logo"><br>Portobook</a>
        
-		<div id="adress"><a href="https://www.google.com/maps/place/Produktdesign,+7034+Trondheim/@63.4159019,10.4116094,14.95z/data=!4m5!3m4!1s0x466d319583c05947:0x88b69f4c40fb20a1!8m2!3d63.4180732!4d10.4049145" target="_blank"> Kolbjørn Hejes vei 2B, 7034 Trondheim, Norge</a></div>
+		<div id="adress"><a href="https://www.google.com/maps/place/Kolbj%C3%B8rn+Hejes+vei+2B,+7034+Trondheim/@63.4180955,10.403451,18z/data=!3m1!4b1!4m5!3m4!1s0x466d31958310c9d1:0x8cd33413e5074057!8m2!3d63.4180943!4d10.4045453" target="_blank"> Kolbjørn Hejes vei 2B, 7034 Trondheim, Norge</a></div>
 		<div id="icons_footer">
 		<div id="facebook_icon"><a href="https://www.facebook.com/"><img src="../images/facebook_icon_footer.png" alt="facebook"></a></div>
 		<div id="instagram_icon"><a href="https://instagram.com"><img src="../images/instagram_icon_footer.png" alt="instagram"></a></div>
