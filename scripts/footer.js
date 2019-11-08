29536ff415b0dc89f20364cff9369b92686198bd
oekeller <55976310+oekeller@users.noreply.github.com>
2019-11-08 20:46:31 +0100

    Footer

--- scripts/footer.js
+++ scripts/footer.js
@@ -2,9 +2,23 @@
 document.getElementById("footer-placeholder").innerHTML = 
     `
         <div id="footer">
-        <p class="contact_information">Kontaktinformasjon:</p>
-        <div id="email"><a href="mailto:portobok@portobok.com"><p class="contact_information">E-post: portobok@portobok.com</p></a></div>
-        <div id="phone"><a href="tel:12345678"><p class="contact_information">Telefon: (+47) 12345678</p></a></div>
-        <div id="adress"><a href="https://www.google.com/maps/place/Produktdesign,+7034+Trondheim/@63.4159019,10.4116094,14.95z/data=!4m5!3m4!1s0x466d319583c05947:0x88b69f4c40fb20a1!8m2!3d63.4180732!4d10.4049145"><p class="contact_information">Adresse: Produktdesign, 7034 Trondheim, Norge</p></a></div>
+		
+		<a href="../html/homepage.html" id="footer_logo"><img src="../images/logo_pb_tykkere.png" alt="Logo"><br>Portobook</a>
+       
+		<div id="adress"><a href="https://www.google.com/maps/place/Produktdesign,+7034+Trondheim/@63.4159019,10.4116094,14.95z/data=!4m5!3m4!1s0x466d319583c05947:0x88b69f4c40fb20a1!8m2!3d63.4180732!4d10.4049145" target="_blank"> Kolbjørn Hejes vei 2B, 7034 Trondheim, Norge</a></div>
+		<div id="icons_footer">
+		<div id="facebook_icon"><a href="https://www.facebook.com/"><img src="../images/facebook_icon_footer.png" alt="facebook"></a></div>
+		<div id="instagram_icon"><a href="https://instagram.com"><img src="../images/instagram_icon_footer.png" alt="instagram"></a></div>
+
+		</div>
+
+        <div id="email"><p class="contact_information">E-post</p><a href="mailto:portobok@portobok.com">portobok@portobok.com</a></div>
+
+        <div id="phone"><p class="contact_information"> Telefon</p><a href="tel:12345678">(+47) 12345678</a></div>
+
+		<div id="kontakt_designere"><p class="contact_information">Kontakt designere</p><a href="../html/contact.html">Her</a></div>
+
         </div>
     `
+/*<p class="contact_information">Kontaktinformasjon:</p>*/
+
